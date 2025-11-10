40b6abc1fd87e7c41edccb46d95cf2b801141203
Milton <ajaxmilton@hotmail.com>
2025-11-10 22:05:41 +0200

    Update styling and functionality for image carousel; change button IDs, add image, and implement navigation logic

--- 002-image-carousel/js/main.js
+++ 002-image-carousel/js/main.js
@@ -1,1 +1,46 @@
-console.log("I m connected!");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+const displayedImage = document.querySelector("displayed-img");
+
+const gallery = [
+  {
+    id: 1,
+    img: "images/image-1.jpg",
+  },
+  {
+    id: 2,
+    img: "images/image-2.jpg",
+  },
+  {
+    id: 3,
+    img: "images/image-3.jpg",
+  },
+  {
+    id: 4,
+    img: "images/image-4.jpg",
+  },
+  {
+    id: 5,
+    img: "images/image-5.jpg",
+  },
+  {
+    id: 6,
+    img: "images/image-6.jpg",
+  },
+  {
+    id: 7,
+    img: "images/image-7.jpg",
+  },
+  {
+    id: 8,
+    img: "images/image-8.jpg",
+  },
+];
+
+prev.addEventListener("click", () => {
+  console.log("prev");
+});
+
+next.addEventListener("click", () => {
+  console.log("next");
+});
