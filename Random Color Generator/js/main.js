7a2eb92920f37ed41a2c2231e6926e71f9a0459a
Milton <ajaxmilton@hotmail.com>
2025-11-10 20:16:20 +0200

    Create a random color generator project for beginners

---
+++ Random Color Generator/js/main.js
@@ -0,0 +1,50 @@
+const hexCode = document.querySelector("#hex-code");
+const generateColorBtn = document.querySelector("#generate-color-btn");
+const displayColor = document.querySelector(".display-color");
+
+const alphaNumerals = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+let hexColor = [];
+
+displayColor.style.backgroundColor =
+  localStorage.getItem("generatedColor") || "red";
+
+hexCode.innerHTML = localStorage.getItem("generatedColor") || "#FF0000";
+
+const generateHexColor = () => {
+  for (let num = 0; num < 6; num++) {
+    const selectedHex = Math.round(Math.random() * alphaNumerals.length - 1);
+    const hexChar = alphaNumerals[selectedHex];
+    hexColor = [...hexColor, hexChar];
+  }
+
+  return `#${hexColor.toString().replaceAll(",", "")}`;
+};
+
+generateColorBtn.addEventListener("click", () => {
+  hexColor = [];
+  const generatedColor = generateHexColor();
+
+  localStorage.setItem("generatedColor", generatedColor);
+
+  displayColor.style.backgroundColor = localStorage.getItem("generatedColor");
+
+  hexCode.innerHTML = generatedColor;
+});
