a8c4ecb10e564a236aa2c65bfa485a55cd14bf3c
Milton <ajaxmilton@hotmail.com>
2025-11-11 12:13:16 +0200

    Refactor todo list application: update styles for consistency and enhance functionality with dynamic todo item addition

--- 003-todo-list/js/main.js
+++ 003-todo-list/js/main.js
@@ -1,1 +1,18 @@
-console.log("I m connected!");
+const todoItem = document.querySelector("#todo");
+const todosContainer = document.querySelector("#todos");
+
+todoItem.addEventListener("change", (e) => {
+  const todoItemHtml = `
+            <li class="todo-list-item">
+                <input type="checkbox" name="todoListItem" id="todoListItem" />
+                <span class="todo-text">${e.target.value}</span>
+            </li>
+`;
+
+  todosContainer.insertAdjacentHTML("beforeend", todoItemHtml);
+  todo.value = "";
+
+  const todoListItem = document.querySelectorAll("#todoListItem");
+
+  console.log(todoListItem);
+});
